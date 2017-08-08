bc3b5488bf624c4e76fb0bf45ffa2535e7151fdd
Allie Grampa <agrampa@yahoo.com>
2017-08-08 15:01:08 -0700

    added basic navbar, library, and shelf components

---
+++ src/component/library/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Library extends React.Component {
+  render() {
+    return(
+      <p>The sass has arrived in the library component</p>
+    )
+  }
+}
+
+export default Library
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import Navbar from './component/navbar'
+import Shelf from './component/shelf'
+import Library from './component/library'
 
 class App extends React.Component {
   constructor(props){
@@ -8,13 +13,19 @@ class App extends React.Component {
 
   render(){
     return(
-      <div>
-        <h1> Here comes the sass. </h1>
-      </div>
+      <main>
+        <BrowserRouter>
+          <div>
+            <h1> Here comes the sass. </h1>
+            <Navbar />
+            <Route exact path='/library' component={Library} />
+            <Route exact path='/shelf' component={Shelf} />
+          </div>
+        </BrowserRouter>
+      </main>
+
     )
   }
-
 }
-
 
 ReactDom.render(<App />, document.getElementById('root'))
