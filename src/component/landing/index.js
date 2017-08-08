cfb3138abcf01bd9af48193529760b01905388f6
Allie Grampa <agrampa@yahoo.com>
2017-08-08 16:47:19 -0700

    added story form and signup

---
+++ src/component/landing/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import superagent from 'superagent'
+
+const API_URL = 'http://localhost:3000'
+
+class SignupForm extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: props.user ? props.user.username : '',
+      password: props.user ? props.user.password : '',
+      email: props.user ? props.user.email : '',
+    }
+  }
+
+  render(){
+    <form id='signup-form'
+      onSubmit={this.handleSubmit}>
+      <input name='username' type='text' value={this.state.username} onChange={this.handleChange} />
+      <input name='password' type='password' value={this.state.password} onChange={this.handleChange} />
+      <input name='email' type='text' value={this.state.email} onChange={this.handleChange} />
+      <button type='submit'>sign up</button>
+    </form>
+  }
+}
+
+class SigninForm extends React.Component {
+
+}
+
+class Landing extends React.Component {
+  
+}
+
+export default Landing
